d41e5c3078469bb6cf3af424721a60aa52e9eb98
Nekkolly <nicolly.carvalho.corp@gmail.com>
2024-05-01 20:04:46 -0300

    Add database dependency and config Error Messages Controller and DTO

--- src/main/java/br/com/nekkolly/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,16 +2,29 @@ package br.com.nekkolly.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CandidateEntity {
-  
+
   private UUID id;
   private String name;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
   private String username;
+
+  @Email(message = "O campo (email) deve conter um e-mail válido")
   private String email;
+
+  @Length(min = 8, max = 20, message = "A senha deve conter entre (10) e (20) caracteres")
   private String password;
+
   private String description;
   private String curriculum;
 }
