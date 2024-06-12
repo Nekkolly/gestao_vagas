985e8eaab0c0d3db8e00da786c0f9a2edfa5aecf
Nekkolly <nicolly.carvalho.corp@gmail.com>
2024-06-11 23:11:48 -0400

    feat: add findUsername in repository and validate in usecase password matches

--- src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -1,12 +1,35 @@
 package br.com.nekkolly.gestao_vagas.modules.company.usecases;
 
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import br.com.nekkolly.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.nekkolly.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
   
-  public void execute() {
-    
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+      .orElseThrow(() -> {
+        throw new UsernameNotFoundException("Company not found");
+      });
+
+    var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+    if(!passwordMatches) {
+      throw new AuthenticationException();
+    }
   }
 
 }
