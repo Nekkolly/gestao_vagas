bafc203667a5100b6b032c236c961eccdbb6c9eb
Nekkolly <nicolly.carvalho.corp@gmail.com>
2024-06-11 23:03:20 -0400

    Add Bean to encript password

---
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -0,0 +1,12 @@
+package br.com.nekkolly.gestao_vagas.modules.company.usecases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCompanyUseCase {
+  
+  public void execute() {
+    
+  }
+
+}
--- src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.nekkolly.gestao_vagas.modules.company.usecases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.nekkolly.gestao_vagas.exceptions.UserFoundException;
@@ -13,6 +14,9 @@ public class CreateCompanyUseCase {
   @Autowired
   private CompanyRepository companyRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CompanyEntity execute(CompanyEntity companyEntity) {
 
     this.companyRepository
@@ -21,6 +25,9 @@ public class CreateCompanyUseCase {
         throw new UserFoundException();
       });
 
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
+    
     return this.companyRepository.save(companyEntity);
   }
 }
