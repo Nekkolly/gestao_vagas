b94c65ed97fc24692f0e699d8fefa27a04d231c1
Nekkolly <nicolly.carvalho.corp@gmail.com>
2024-06-13 22:26:40 -0400

    Add: security filter

--- src/main/java/br/com/nekkolly/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -2,10 +2,8 @@ package br.com.nekkolly.gestao_vagas.modules.company.controllers;
 
 import javax.security.sasl.AuthenticationException;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.nekkolly.gestao_vagas.modules.company.usecases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +46,7 @@ public class AuthCompanyUseCase {
     // Se for igual -> Gerar o token
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
     var token = JWT.create().withIssuer("javagas")
+      .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
       .withSubject(company.getId().toString())
       .sign(algorithm);
     
