a721418153d8b5875f06c2918d80601d40ee8130
Nekkolly <nicolly.carvalho.corp@gmail.com>
2024-06-29 22:28:35 -0300

    feat: create controller to authenticate candidate

--- src/main/java/br/com/nekkolly/gestao_vagas/modules/candidate/usecases/AuthCandidateUseCase.java
+++ src/main/java/br/com/nekkolly/gestao_vagas/modules/candidate/usecases/AuthCandidateUseCase.java
@@ -45,6 +45,7 @@ public class AuthCandidateUseCase {
     }
 
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
     var token = JWT.create()
       .withIssuer("javagas")
       .withSubject(candidate.getId().toString())
@@ -54,6 +55,7 @@ public class AuthCandidateUseCase {
 
     var authCandidateResponse = AuthCandidateResponseDTO.builder()
       .access_token(token)
+      .expires_in(expiresIn.toEpochMilli())
       .build();
 
     return authCandidateResponse;
